3cb4a07cef9e35500af1122ded3711d0e68574f1
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-01-25 16:42:12 -0600

    created javascript files - internal and external for exercises

---
+++ js/external.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('Hello from external JavaScript');
+
+alert('Welcome to my webpage!');
+
+var userFavColor = prompt('What is your favorite color?');
+console.log(alert('Great! That\'s a fantastic color! ' + userFavColor + ' is my favorite color also!'));
+
+
+
+// Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color entered is your favorite color too.
+//
+//     For example, if the user enters "blue", your code should alert a message that says:
+//
+//     "Great, blue is my favorite color too!"
