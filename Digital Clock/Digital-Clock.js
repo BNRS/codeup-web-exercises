09bb3b1b048c07430622939f759da0c03c157720
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-02-04 17:08:14 -0600

    working on clock using JS

--- Digital Clock/Digital-Clock.js
+++ Digital Clock/Digital-Clock.js
@@ -1,27 +1,33 @@
-"use strict";
+(function() {
 
-function clock() {
 
-    var fullDate = new Date();
-    var hour = fullDate.getHours();
-    var min = fullDate.getMinutes();
-    var sec = fullDate.getSeconds();
+    "use strict";
 
-    if (hour < 10) {
-        hour = "0" + hour;
+
+    function clock() {
+
+        var fullDate = new Date();
+        var hour = fullDate.getHours();
+        var min = fullDate.getMinutes();
+        var sec = fullDate.getSeconds();
+
+        if (hour < 10) {
+            hour = "0" + hour;
+        }
+        if (min < 10) {
+            min = "0" + min;
+        }
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+
+        document.getElementById('hours').innerHTML = hour;
+        document.getElementById('minutes').innerHTML = ": " + min;
+        document.getElementById('seconds').innerHTML = ": " + sec;
+
     }
-    if (min < 10) {
-        min = "0" + min;
-    }
-    if (sec < 10) {
-        sec = "0" + sec;
-    }
-
-    document.getElementById('hours').innerHTML = hour;
-    document.getElementById('minutes').innerHTML = ": " + min;
-    document.getElementById('seconds').innerHTML = ": " + sec;
-
-}
 
 
-setInterval(clock, 100);
+    setInterval(clock, 100);
+
+})();
