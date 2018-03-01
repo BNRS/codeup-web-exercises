df14789bffc68e11a66afeb07c4934096b679e5a
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-03-01 09:22:23 -0600

    es6 promises and syntax

--- js/promises.js
+++ js/promises.js
@@ -15,15 +15,16 @@ wait(3300).then((data) => console.log(data));
 function getDateOfLastCommit(username) {
     let dates = [];
 
-    fetch('https://api.github.com/users/' + username + "/repos?per_page=100", {headers: {'Authorization': 'token 3c1ac3cd112d394e2094221c4fdf6cc83a80a366'}})
+    fetch('https://api.github.com/users/' + username + "/events/public", {headers: {'Authorization': 'token 1f656338837a0df49c62dee66f0f09b0fc643a5a'}})
         .then(response => response.json())
         .then(repositories => {
             repositories.map(repo => {
-                dates.push(repo.updated_at);
+                dates.push(repo.created_at);
             });
             dates.sort();
             document.querySelector('span').innerText = new Date(dates[dates.length -1]);
+            console.log(dates)
         });
 }
 
-getDateOfLastCommit("ryanorsinger");
+getDateOfLastCommit("BNRS");
